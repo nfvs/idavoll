3880f85e7b8b431942c439110751a8ee3ae77b93
Nuno Santos <nunofvsantos@gmail.com>
2010-09-03 14:56:23 +0100

    bugfixes in tap.py

--- idavoll/tap.py
+++ idavoll/tap.py
@@ -14,84 +14,91 @@ from idavoll import __version__
 from idavoll.backend import BackendService
 
 class Options(usage.Options):
-    optParameters = [
-        ('jid', None, 'pubsub', 'JID this component will be available at'),
-        ('secret', None, 'secret', 'Jabber server component secret'),
-        ('rhost', None, '127.0.0.1', 'Jabber server host'),
-        ('rport', None, '5347', 'Jabber server port'),
-        ('backend', None, 'memory', 'Choice of storage backend'),
-        ('dbuser', None, None, 'Database user (pgsql backend)'),
-        ('dbname', None, 'pubsub', 'Database name (pgsql backend)'),
-        ('dbpass', None, None, 'Database password (pgsql backend)'),
-        ('dbhost', None, None, 'Database host (pgsql backend)'),
-        ('dbport', None, None, 'Database port (pgsql backend)'),
-    ]
+	optParameters = [
+		('jid', None, 'pubsub', 'JID this component will be available at'),
+		('secret', None, 'secret', 'Jabber server component secret'),
+		('rhost', None, '127.0.0.1', 'Jabber server host'),
+		('rport', None, '5347', 'Jabber server port'),
+		('backend', None, 'memory', 'Choice of storage backend'),
+		('dbuser', None, None, 'Database user (pgsql backend)'),
+		('dbname', None, 'pubsub', 'Database name (pgsql backend)'),
+		('dbpass', None, None, 'Database password (pgsql backend)'),
+		('dbhost', None, None, 'Database host (pgsql backend)'),
+		('dbport', None, None, 'Database port (pgsql backend)'),
+	]
 
-    optFlags = [
-        ('verbose', 'v', 'Show traffic'),
-        ('hide-nodes', None, 'Hide all nodes for disco')
-    ]
+	optFlags = [
+		('verbose', 'v', 'Show traffic'),
+		('hide-nodes', None, 'Hide all nodes for disco')
+	]
 
-    def postOptions(self):
-        if self['backend'] not in ['pgsql', 'memory']:
-            raise usage.UsageError, "Unknown backend!"
+	def postOptions(self):
+		if self['backend'] not in ['pgsql', 'memory', 'couchdb']:
+			raise usage.UsageError, "Unknown backend!"
 
-        self['jid'] = JID(self['jid'])
+		self['jid'] = JID(self['jid'])
 
 def makeService(config):
-    s = service.MultiService()
+	s = service.MultiService()
 
-    # Create backend service with storage
+	# Create backend service with storage
 
-    if config['backend'] == 'pgsql':
-        from twisted.enterprise import adbapi
-        from idavoll.pgsql_storage import Storage
-        dbpool = adbapi.ConnectionPool('psycopg2',
-                                       user=config['dbuser'],
-                                       password=config['dbpass'],
-                                       database=config['dbname'],
-                                       host=config['dbhost'],
-                                       port=config['dbport'],
-                                       cp_reconnect=True,
-                                       #client_encoding='utf-8',
-                                       )
-        st = Storage(dbpool)
-    elif config['backend'] == 'memory':
-        from idavoll.memory_storage import Storage
-        st = Storage()
+	if config['backend'] == 'pgsql':
+		from twisted.enterprise import adbapi
+		from idavoll.pgsql_storage import Storage
+		dbpool = adbapi.ConnectionPool('psycopg2',
+									   user=config['dbuser'],
+									   password=config['dbpass'],
+									   database=config['dbname'],
+									   host=config['dbhost'],
+									   port=config['dbport'],
+									   cp_reconnect=True,
+									   #client_encoding='utf-8',
+									   )
+		st = Storage(dbpool)
+	elif config['backend'] == 'memory':
+		from idavoll.memory_storage import Storage
+		st = Storage()
 
-    elif config['backend'] == 'couchdb':
-        from idavoll.couchdb_storage import Storage
+	elif config['backend'] == 'couchdb':
+		from idavoll.couchdb_storage import Storage
+		from restkit import SimplePool
+		from couchdbkit import Server
 		pool = SimplePool(keepalive=5) # pool of 5 connections to couchdb
 		server = Server(
-			'http://%s:%s/' % (config['dbhost'], config['dbport'])
+			'http://%s:%s/' % (config['dbhost'], config['dbport']),
 			pool_instance=pool)
 		db = server.get_or_create_db('pubsub')
-		st = Storage(self.db)
+		st = Storage(db)
+		
+		# upload design docs
+		from couchdbkit.loaders import FileSystemDocsLoader
+		loader = FileSystemDocsLoader('db/couchdb')
+		loader.sync(db)
 
-    bs = BackendService(st)
-    bs.setName('backend')
-    bs.setServiceParent(s)
+	bs = BackendService(st)
+	bs.setName('backend')
+	bs.setServiceParent(s)
 
-    # Set up XMPP server-side component with publish-subscribe capabilities
+	# Set up XMPP server-side component with publish-subscribe capabilities
 
-    cs = Component(config["rhost"], int(config["rport"]),
-                   config["jid"].full(), config["secret"])
-    cs.setName('component')
-    cs.setServiceParent(s)
+	cs = Component(config["rhost"], int(config["rport"]),
+				   config["jid"].full(), config["secret"])
+	cs.setName('component')
+	cs.setServiceParent(s)
 
-    cs.factory.maxDelay = 900
+	cs.factory.maxDelay = 900
 
-    if config["verbose"]:
-        cs.logTraffic = True
+	if config["verbose"]:
+		cs.logTraffic = True
 
-    FallbackHandler().setHandlerParent(cs)
-    VersionHandler('Idavoll', __version__).setHandlerParent(cs)
-    DiscoHandler().setHandlerParent(cs)
+	FallbackHandler().setHandlerParent(cs)
+	VersionHandler('Idavoll', __version__).setHandlerParent(cs)
+	DiscoHandler().setHandlerParent(cs)
 
-    ps = IPubSubService(bs)
-    ps.setHandlerParent(cs)
-    ps.hideNodes = config["hide-nodes"]
-    ps.serviceJID = config["jid"]
+	ps = IPubSubService(bs)
+	ps.setHandlerParent(cs)
+	ps.hideNodes = config["hide-nodes"]
+	ps.serviceJID = config["jid"]
 
-    return s
+	return s
